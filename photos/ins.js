f49241b2dad368e95ab9d8ccb60540ba1455d7a7
Yangtao <243640506@qq.com>
2019-12-07 01:07:59 +0800

    Site updated: 2019-12-07 01:07:59

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://raw.githubusercontent.com/zytlegend/zytlegend.github.io/master/min_photos/' + data.link[i];
-          var src = 'https://raw.githubusercontent.com/zytlegend/zytlegend.github.io/master/photos/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/zytlegend/Blog-back-up/master/min_photos/' + data.link[i];
+          var src = 'https://raw.githubusercontent.com/zytlegend/Blog-back-up/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
